a40c2d728dc8b920e1367e3fbbce56588fe886c0
Water <nirvanjain@icloud.com>
2025-06-02 18:16:32 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1106,5 +1106,8 @@ app.post('/flashcard', (req, res) => {
 
 
 
+// Ensure your server actually starts
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🌐 Server running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
