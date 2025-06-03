e3862e050892174d4676c3cb8588047f54d5d48e
Water <nirvanjain@icloud.com>
2025-06-02 20:13:49 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4,13 +4,15 @@ const express     = require('express');
 const cors        = require('cors');
 const killTrigger = require('./middleware/killTrigger');
 const admin = require("firebase-admin");
-const serviceAccount = require("./firebase.json");
 
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-  });
-}
+admin.initializeApp({
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  }),
+});
+
 
 
 // ── Express & OpenAI clients ──
